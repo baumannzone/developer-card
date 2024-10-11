1f6d5657eb6b50343b7711ae27ead4081501e353
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2024-10-11 23:53:37 +0200

    v2

--- index.js
+++ index.js
@@ -4,49 +4,61 @@ import boxen from "boxen";
 import chalk from "chalk";
 
 const boxenOptions = {
-  margin: .5,
+  margin: 1,
   padding: 1,
-  width: 49,
+  width: 53,
   title: "@baumannzone",
   titleAlignment: "center",
-  borderStyle: "round",
-  borderColor: "#8c52ff",
+  borderStyle: "bold",
+  borderColor: "yellow",
 };
 
-const intro = chalk("Hi 👋! I'm Jorge Baumann ") + chalk.italic("aka ") + chalk.bold.underline.bgMagentaBright("Baumannzone") + chalk(". I help developers to grow their careers and build amazing software.\n\nFind me on the internet.\n\n");
+const intro = chalk("Hi! I'm ")
+  + chalk.bold.underline.bgYellowBright("Jorge Baumann")
+  + chalk(". I help developers grow their careers and build amazing software.\n\nFind me on the internet.\n\n");
 
 const links = [
   {
     name: chalk.hex("#fff").bgHex("#0f111a")("Web"),
-    url: "     https://baumannzone.dev"
+    url: "      https://baumannzone.dev"
   },
   {
     name: chalk.hex("#fff").bgHex("#1DA1F2")("𝕏"),
-    url: "       https://twitter.com/baumannzone",
+    url: "        https://x.com/baumannzone",
   },
 
   { name: chalk.hex("#fff").bgHex("#9146ff")("Twitch"), 
-    url: "  https://twitch.tv/baumannzone"
+    url: "   https://twitch.tv/baumannzone"
   },
   
   {
     name: chalk.hex("#fff").bgHex("#6e5494")("GitHub"),
-    url: "  https://github.com/baumannzone",
+    url: "   https://github.com/baumannzone",
   },
   
   {
+    name: chalk.hex("#fff").bgHex("#000")("CodePen"),
+    url: "  https://codepen.io/baumannzone",
+  },
+
+  {
     name: chalk.hex("#fff").bgHex("#ee1d52")("Tik") + chalk.hex("#000").bgHex("#69c9d0")("Tok"),
-    url: "  https://tiktok.com/@baumannzone",
+    url: "   https://tiktok.com/@baumannzone",
   },
 
   { name: chalk.hex("#fff").bgHex("#000")("Threads"), 
-    url: " https://threads.net/baumannzone"
+    url: "  https://threads.net/baumannzone"
   },
   
   {
     name: chalk.hex("#fff").bgHex("#ff0000")("YouTube"),
-    url: " https://youtube.com/@baumannzone",
+    url: "  https://youtube.com/@baumannzone",
   },
+
+  {
+    name: chalk.hex("#0077b5").bgHex("#fff")("Linked") + chalk.hex("#fff").bgHex("#0077b5")("In"),
+    url: " https://linkedin.com/in/baumannzone",
+  }
 ];
 
 const linkList = links.map((link) => `${link.name} ${link.url}`).join("\n");
