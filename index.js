1025e7a72acbba6da6c136d7b12853590c59f793
Guillermo Rodas <glrodasz@gmail.com>
2025-02-05 19:23:42 +0100

    Allow to pass a custom handle

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 import boxen from "boxen";
 import chalk from "chalk";
 
+// Get handle from command line args or use default
+const handle = process.argv[2] || 'baumannzone';
+
 const boxenOptions = {
   margin: 1,
   padding: 1.5,
   width: 56,
-  title: "@baumannzone",
+  title: `@${handle}`,
   titleAlignment: "center",
   borderStyle: "bold",
   borderColor: "yellowBright",
@@ -20,44 +23,44 @@ const intro = chalk("Hi! I'm ")
 const links = [
   {
     name: chalk.hex("#fff").bgHex("#0f111a")("Web"),
-    url: chalk.dim("      https://") + "baumannzone.dev"
+    url: chalk.dim("      https://") + `${handle}.dev`
   },
   {
     name: chalk.hex("#fff").bgHex("#1DA1F2")("𝕏"),
-    url: chalk.dim("        https://x.com/") + "baumannzone",
+    url: chalk.dim("        https://x.com/") + handle,
   },
 
   { name: chalk.hex("#fff").bgHex("#9146ff")("Twitch"), 
-    url: chalk.dim("   https://twitch.tv") + "/baumannzone"
+    url: chalk.dim("   https://twitch.tv/") + handle
   },
   
   {
     name: chalk.hex("#fff").bgHex("#6e5494")("GitHub"),
-    url: chalk.dim("   https://github.com") + "/baumannzone",
+    url: chalk.dim("   https://github.com") + "/" + handle,
   },
   
   {
     name: chalk.hex("#fff").bgHex("#000")("CodePen"),
-    url: chalk.dim("  https://codepen.io") + "/baumannzone",
+    url: chalk.dim("  https://codepen.io") + "/" + handle,
   },
 
   {
     name: chalk.hex("#fff").bgHex("#ee1d52")("Tik") + chalk.hex("#000").bgHex("#69c9d0")("Tok"),
-    url: chalk.dim("   https://tiktok.com") + "/@baumannzone",
+    url: chalk.dim("   https://tiktok.com") + "/@" + handle,
   },
 
   { name: chalk.hex("#fff").bgHex("#000")("Threads"), 
-    url: chalk.dim("  https://threads.net") + "/baumannzone"
+    url: chalk.dim("  https://threads.net") + "/" + handle
   },
   
   {
     name: chalk.hex("#fff").bgHex("#ff0000")("YouTube"),
-    url: chalk.dim("  https://youtube.com") + "/@baumannzone",
+    url: chalk.dim("  https://youtube.com") + "/@" + handle,
   },
 
   {
     name: chalk.hex("#0077b5").bgHex("#fff")("Linked") + chalk.hex("#fff").bgHex("#0077b5")("In"),
-    url: chalk.dim(" https://linkedin.com/in") + "/baumannzone",
+    url: chalk.dim(" https://linkedin.com/in") + "/" + handle,
   }
 ];
 
